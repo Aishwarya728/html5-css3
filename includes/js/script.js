367313b6e6474542c0c075b5e3b6af5b2379363a
Aishwarya Jaiswal <aishwarya.vivian@gmail.com>
2017-09-10 20:47:47 +0530

    Website using bootstrap

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,15 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+$(function(){
+
+	$('a.pop').click(function(e){
+		e.preventDefault();
+		});
+
+	$('a.pop').popover();
+
+	$('[rel="tooltip"]').tooltip();
+
+});
